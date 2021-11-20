afa14b064a77b24451850b7205d995ce8ae964a1
EdisonAliu1 <ea48304@ubt-uni.net>
2021-11-20 01:14:05 +0100

    Initial Commit

---
+++ DristRent/DristRent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DristRent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
